15fb59405ffd9cd8f8e9e3b0b1063ec7390c42af
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-09 22:00:04 +0100

    leading state for map

--- components/Maps/BrunnenbauMap.vue
+++ components/Maps/BrunnenbauMap.vue
@@ -2,9 +2,8 @@
 	const props = defineProps({
 		data: Object,
 	});
-	const { $gsap } = useNuxtApp();
-	let mm = $gsap.matchMedia();
 	const coordinates = ref([]);
+	let isReady = ref(false);
 
 	function convertDMSToDD(degrees, minutes, seconds, direction) {
 		var dd =
@@ -39,30 +38,38 @@
 				});
 			}
 		});
-
-		mm.add('(min-width: 768px)', () => {
-			return () => {};
-		});
 	});
 </script>
 <template>
-	<LMap
-		style="height: 500px"
-		:zoom="9"
-		:center="[3.848, 11.5021]"
-		:use-global-leaflet="false">
-		<LTileLayer
-			url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-			layer-type="base"
-			name="OpenStreetMap" />
-		<LMarker
-			v-for="(coord, index) in coordinates"
-			:key="index"
-			:lat-lng="[coord.lat, coord.lon]"
-			><LPopup>
-				{{ data.stories[index].content.location }}
-			</LPopup></LMarker
-		>
-	</LMap>
+	<div class="relative">
+		<LMap
+			style="height: 500px"
+			:zoom="9"
+			@ready="
+				() => {
+					isReady = true;
+				}
+			"
+			:center="[3.848, 11.5021]"
+			:use-global-leaflet="false">
+			<LTileLayer
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+				layer-type="base"
+				name="OpenStreetMap" />
+			<LMarker
+				v-for="(coord, index) in coordinates"
+				:key="index"
+				:lat-lng="[coord.lat, coord.lon]"
+				><LPopup>
+					{{ data.stories[index].content.location }}
+				</LPopup></LMarker
+			>
+		</LMap>
+		<div
+			class="h-[500px] bg-white absolute inset-0 grid place-items-center"
+			v-if="!isReady">
+			<span class="loading loading-spinner loading-lg"></span>
+		</div>
+	</div>
 </template>
