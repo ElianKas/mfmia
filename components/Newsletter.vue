195e6b19e6bd24bddf2509321bdb89070c9fe571
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-10 12:54:55 +0100

    Update Newsletter.vue

--- components/Newsletter.vue
+++ components/Newsletter.vue
@@ -3,7 +3,28 @@
 		<SvgsHumansGroup class="w-full h-auto" />
 		<div
 			class="w-full border rounded-[--border-radius] border-orange h-[500px]">
-			block
+			<p>Zum Newsletter Anmelden und nicht mehr verpassen!</p>
+			<label class="input input-bordered flex items-center gap-2">
+				Email
+				<input
+					type="text"
+					class="grow" />
+			</label>
+			<label class="input input-bordered flex items-center gap-2">
+				Email
+				<input
+					type="text"
+					class="grow" />
+			</label>
+			<div class="form-control">
+				<label class="label cursor-pointer">
+					<input
+						type="checkbox"
+						:checked="true"
+						class="checkbox" />
+					<span class="label-text">Remember me</span>
+				</label>
+			</div>
 		</div>
 		<SvgsHumansGroup class="w-full h-auto" />
 	</aside>
