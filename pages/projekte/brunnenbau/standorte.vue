fb019ddf8ef6daccf500efa835fe94bb1d14d88e
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-12 10:58:41 +0100

    Update standorte.vue

--- pages/projekte/brunnenbau/standorte.vue
+++ pages/projekte/brunnenbau/standorte.vue
@@ -41,7 +41,7 @@
 		</section>
 		<section class="px-[3rem] mt-[3rem]">
 			<div
-				class="flex flex-wrap justify-between gap-[1rem] py-[1rem] md:border-b border-orange">
+				class="flex flex-wrap justify-between gap-[1rem] py-[1rem] md:border-b border-orange font-bold">
 				<h2 class="text-orange">Projekte - Brunnenbau Karte</h2>
 				<div v-if="data && data.stories">
 					{{ data.stories.length }} Standorte
@@ -50,27 +50,34 @@
 			</div>
 			<ClientOnly>
 				<template #fallback>
-					<div class="grid grid-rows-6 gap-[1rem] my-[1rem]">
-						<div class="skeleton h-[24px]"></div>
-						<div class="skeleton h-[24px]"></div>
-						<div class="skeleton h-[24px]"></div>
-						<div class="skeleton h-[24px]"></div>
-						<div class="skeleton h-[24px]"></div>
-						<div class="skeleton h-[24px]"></div>
+					<div class="mt-[1rem]">
+						<div
+							class="skeleton h-[42px] md:h-[27px] mb-[1rem]"
+							v-for="entry in data.stories.length"></div>
 					</div>
 				</template>
 				<div
-					class="grid-cols-6 my-[1rem] gap-[1rem] hidden lg:grid"
+					class="grid-cols-[50px_1fr_1fr_1fr_1fr_50px] my-[1rem] gap-[1rem] hidden lg:grid"
 					v-if="data && data.stories"
 					v-for="(story, index) in data?.stories || []">
-					<div>
+					<div class="font-bold">
 						{{ data.stories.length - index }}
 					</div>
-					<div>{{ story.content.location }}</div>
-					<div>{{ story.content.well }}</div>
-					<div>{{ story.content.coordinates }}</div>
-					<div>{{ story.content.group }}</div>
-					<div>{{ story.content.year }}</div>
+					<div class="font-bold">
+						{{ story.content.location }}
+					</div>
+					<div>
+						{{ story.content.well }}
+					</div>
+					<div>
+						{{ story.content.coordinates }}
+					</div>
+					<div>
+						{{ story.content.group }}
+					</div>
+					<div class="justify-self-end">
+						{{ story.content.year }}
+					</div>
 				</div>
 				<div
 					class="collapse collapse-arrow border-t rounded-none lg:hidden"
@@ -79,7 +86,7 @@
 					<input
 						type="radio"
 						name="my-accordion-1" />
-					<div class="collapse-title pl-[1rem]">
+					<div class="collapse-title px-0">
 						{{ data.stories.length - index }}
 						{{ story.content.location }}
 					</div>
