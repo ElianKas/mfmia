81ff39b64571f6abfb2f3a18aaa9d67932f47a36
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-16 18:06:46 +0100

    Update presse.vue

--- pages/news/presse.vue
+++ pages/news/presse.vue
@@ -1,1 +1,25 @@
-<template><div>presse</div></template>
+<script setup>
+	const storyblokApi = useStoryblokApi();
+	const { data } = await storyblokApi.get('cdn/stories', {
+		version: useRoute().query._storyblok ? 'draft' : 'published',
+		starts_with: 'news/aktuelles',
+		is_startpage: false,
+	});
+	const sortedData = ref(null);
+
+	onMounted(() => {
+		sortedData.value = data.stories.sort((a, b) => {
+			const dateA = new Date(a.content.date);
+			const dateB = new Date(b.content.date);
+			return dateB - dateA;
+		});
+	});
+</script>
+<template>
+	<article class="px-[1rem]">
+		<div
+			class="h-[200px] bg-[#000] w-full rounded-[--border-radius] grid place-items-center my-[1rem]">
+			<h1 class="text-white text-big">Presseberichte</h1>
+		</div>
+	</article>
+</template>
