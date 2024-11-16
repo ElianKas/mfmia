12622280136b68067fd877f6ca3b8d50eab23d3a
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-16 21:09:53 +0100

    projekte index

---
+++ components/Svgs/Navigation/DoubleArrowWhite.vue
@@ -0,0 +1,22 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="17.646"
+		height="14.629"
+		viewBox="0 0 17.646 14.629">
+		<g transform="translate(0.707 0.707)">
+			<path
+				d="M-10444.573-4444.833l6.607,6.607-6.607,6.607"
+				transform="translate(10444.573 4444.833)"
+				fill="none"
+				stroke="#fff"
+				stroke-width="2" />
+			<path
+				d="M-10444.573-4444.833l6.607,6.607-6.607,6.607"
+				transform="translate(10453.49 4444.833)"
+				fill="none"
+				stroke="#fff"
+				stroke-width="2" />
+		</g>
+	</svg>
+</template>
--- pages/projekte/index.vue
+++ pages/projekte/index.vue
@@ -1,1 +1,43 @@
-<template><div>projekte</div></template>
+<template>
+	<article>
+		<section class="px-[1rem]">
+			<h1
+				class="font-bold text-orange text-big text-center mt-[5rem]">
+				Lerne unsere Projekte kennen
+			</h1>
+			<nav class="my-[5rem] max-w-[--max-width] m-auto">
+				<ul>
+					<div class="grid grid-cols-3 gap-[1rem]">
+						<li>
+							<div
+								class="bg-[#000] aspect-square flex items-end rounded-[--border-radius]">
+								<NuxtLink
+									to="/projekte/brunnenbau"
+									class="flex items-center justify-between cursor-pointer w-full p-[1.5rem]">
+									<div class="text-white font-bold">Brunnenbau</div>
+									<div>
+										<SvgsNavigationDoubleArrowWhite
+											class="w-[20px] h-auto" />
+									</div>
+								</NuxtLink>
+							</div>
+							<br />
+							<div class="pr-[1rem]">
+								<NuxtLink
+									to="/projekte/brunnenbau"
+									class="flex items-center justify-between cursor-pointer">
+									<div>übersicht</div>
+									<div>
+										<SvgsNavigationDoubleArrow
+											class="w-[20px] h-auto" />
+									</div>
+								</NuxtLink>
+							</div>
+						</li>
+					</div>
+				</ul>
+			</nav>
+		</section>
+		<Newsletter />
+	</article>
+</template>
