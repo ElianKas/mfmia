86d85fb948981a6d82853784e87df06b3dca1a43
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-18 10:31:07 +0100

    Update StoryCard.vue

--- components/Templates/StoryCard.vue
+++ components/Templates/StoryCard.vue
@@ -5,24 +5,23 @@
 	onMounted(() => {});
 </script>
 <template>
-	<ClientOnly>
-		<NuxtLink
-			:to="story.full_slug"
-			class="h-[500px] w-full rounded-[--border-radius] relative">
-			<NuxtImg
-				provider="storyblok"
-				format="avif"
-				placeholder
-				class="rounded-[--border-radius] absolute inset-0 w-full h-full object-cover"
-				:src="story.content.cover.filename"
-				:alt="story.content.title" />
-			<div
-				class="backdrop-blur h-[150px] p-[1rem] absolute text-white bottom-0 w-full rounded-b-[--border-radius] flex flex-col justify-between">
-				<p class="font-bold">
-					{{ story.content.title }}
-				</p>
-				<div class="self-end"><SvgsNavigationShortArrow /></div>
-			</div>
-		</NuxtLink>
-	</ClientOnly>
+	<NuxtLink
+		v-if="story"
+		:to="story.full_slug"
+		class="h-[500px] w-full rounded-[--border-radius] relative">
+		<NuxtImg
+			provider="storyblok"
+			format="avif"
+			placeholder
+			class="rounded-[--border-radius] absolute inset-0 w-full h-full object-cover"
+			:src="story.content.cover.filename"
+			:alt="story.content.title" />
+		<div
+			class="backdrop-blur h-[150px] p-[1rem] absolute text-white bottom-0 w-full rounded-b-[--border-radius] flex flex-col justify-between">
+			<p class="font-bold">
+				{{ story.content.title }}
+			</p>
+			<div class="self-end"><SvgsNavigationShortArrow /></div>
+		</div>
+	</NuxtLink>
 </template>
