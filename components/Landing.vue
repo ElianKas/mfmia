9cff8db39f38465495dbccd29227d6f76c79d6db
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-19 18:24:30 +0100

    Update Landing.vue

--- components/Landing.vue
+++ components/Landing.vue
@@ -1,5 +1,34 @@
 <script setup>
-	let gallery;
+	const { $gsap } = useNuxtApp();
+	const gallery = ref(0);
+	let galleryInterval;
+
+	function handleHover() {
+		clearInterval(galleryInterval);
+	}
+
+	function handleLeave() {
+		galleryInterval = setInterval(() => {
+			if (gallery.value < 2) {
+				gallery.value++;
+			} else {
+				gallery.value = 0;
+			}
+		}, 3000);
+	}
+
+	onMounted(() => {
+		galleryInterval = setInterval(() => {
+			if (gallery.value < 2) {
+				gallery.value++;
+			} else {
+				gallery.value = 0;
+			}
+		}, 3000);
+	});
+	onBeforeUnmount(() => {
+		clearInterval(galleryInterval);
+	});
 </script>
 <template>
 	<section
@@ -9,9 +38,27 @@
 				provider="cloudinary"
 				format="avif"
 				preload
-				fit="cover"
+				@mouseover="handleHover"
+				@mouseleave="handleLeave"
 				src="https://res.cloudinary.com/dxizsaymj/image/upload/v1731083483/IMG_3554_kvdd4f.jpg"
-				class="absolute inset-0 w-full h-full object-cover rounded-[--border-radius]" />
+				class="landing0 absolute inset-0 w-full h-full object-cover rounded-[--border-radius]"
+				:class="{ hidden: gallery !== 0 }" />
+			<NuxtImg
+				provider="cloudinary"
+				format="avif"
+				@mouseover="handleHover"
+				@mouseleave="handleLeave"
+				src="https://res.cloudinary.com/dxizsaymj/image/upload/v1731083487/IMG_3529_y4dlkz.jpg"
+				class="landing1 absolute inset-0 w-full h-full object-cover rounded-[--border-radius]"
+				:class="{ hidden: gallery !== 1 }" />
+			<NuxtImg
+				provider="cloudinary"
+				format="avif"
+				@mouseover="handleHover"
+				@mouseleave="handleLeave"
+				src="https://res.cloudinary.com/dxizsaymj/image/upload/v1731083454/IMG_3611_d9sqye.jpg"
+				class="landing2 absolute inset-0 w-full h-full object-cover rounded-[--border-radius]"
+				:class="{ hidden: gallery !== 2 }" />
 		</div>
 		<br />
 		<SvgsLogoNormal class="w-full h-auto mb-[5rem]" />
