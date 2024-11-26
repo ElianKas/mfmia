218693b5d01fe79670f684b1dc9ade1d3f4f505c
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-26 15:03:59 +0100

    svgs

---
+++ components/Svgs/Content/Houses.vue
@@ -0,0 +1,179 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="214.575"
+		height="167.506"
+		viewBox="0 0 214.575 167.506">
+		<g transform="translate(-1489.074 -2363.609)">
+			<g transform="translate(1520.221 2449.002)">
+				<path
+					d="M-14678.316,17262.037s51.668-27.717,61.814-28.033,56.743,28.033,56.743,28.033"
+					transform="translate(14699.316 -17237.004)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="4" />
+				<line
+					y2="64.774"
+					transform="translate(41.563 15.281)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="4" />
+				<line
+					x2="1.905"
+					y2="64.774"
+					transform="translate(121.578 15.281)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="4" />
+			</g>
+			<g transform="translate(1621.928 2415.535)">
+				<path
+					d="M-14678.316,17247.271s24.455-13.119,29.258-13.268,26.857,13.268,26.857,13.268"
+					transform="translate(14699.316 -17237.004)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="3" />
+				<line
+					y2="30.659"
+					transform="translate(30.733 5.178)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="3" />
+				<path
+					d="M0,0,.9,30.659"
+					transform="translate(68.606 6.178)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="3" />
+			</g>
+			<g transform="translate(1470.105 2409.162)">
+				<path
+					d="M-14678.316,17247.271s24.455-13.119,29.258-13.268,26.857,13.268,26.857,13.268"
+					transform="translate(14699.316 -17237.004)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="3" />
+				<line
+					y2="30.659"
+					transform="translate(30.733 5.178)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="3" />
+				<path
+					d="M0,0,.9,30.659"
+					transform="translate(68.606 6.178)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="3" />
+			</g>
+			<g transform="translate(1580.813 2393.977)">
+				<path
+					d="M-14678.316,17241.795s14.363-7.705,17.184-7.791,15.773,7.791,15.773,7.791"
+					transform="translate(14699.316 -17237.006)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="2" />
+				<line
+					y2="18.006"
+					transform="translate(26.716 1.802)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="2" />
+				<path
+					d="M0,0,.53,18.006"
+					transform="translate(48.959 2.39)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="2" />
+			</g>
+			<g transform="translate(1525.043 2381.277)">
+				<path
+					d="M-14678.316,17241.795s14.363-7.705,17.184-7.791,15.773,7.791,15.773,7.791"
+					transform="translate(14699.316 -17237.006)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="2" />
+				<line
+					y2="18.006"
+					transform="translate(26.716 1.802)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="2" />
+				<path
+					d="M0,0,.53,18.006"
+					transform="translate(48.959 2.39)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="2" />
+			</g>
+			<g transform="translate(1617.01 2367.111)">
+				<path
+					d="M-14678.316,17238.721s8.7-4.666,10.4-4.717,9.55,4.717,9.55,4.717"
+					transform="translate(14699.316 -17237.006)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="1" />
+				<line
+					y2="10.901"
+					transform="translate(24.461 -0.093)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="1" />
+				<path
+					d="M0,0,.321,10.9"
+					transform="translate(37.927 0.262)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="1" />
+			</g>
+			<g transform="translate(1662.01 2374.194)">
+				<path
+					d="M-14678.316,17238.721s8.7-4.666,10.4-4.717,9.55,4.717,9.55,4.717"
+					transform="translate(14699.316 -17237.006)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="1" />
+				<line
+					y2="10.901"
+					transform="translate(24.461 -0.093)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="1" />
+				<path
+					d="M0,0,.321,10.9"
+					transform="translate(37.927 0.262)"
+					fill="none"
+					stroke="#eb6b13"
+					stroke-linecap="round"
+					stroke-width="1" />
+			</g>
+			<path
+				d="M-895.427,14738.546h81.974"
+				transform="translate(2457 -12209.488)"
+				fill="none"
+				stroke="#eb6b13"
+				stroke-linecap="round"
+				stroke-width="4" />
+		</g>
+	</svg>
+</template>
