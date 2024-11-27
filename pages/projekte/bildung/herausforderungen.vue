c3bf06b10883ad802ecd2a170f510d8032048123
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-27 15:35:20 +0100

    Update herausforderungen.vue

--- pages/projekte/bildung/herausforderungen.vue
+++ pages/projekte/bildung/herausforderungen.vue
@@ -1,1 +1,24 @@
-<template><div>herausforderungen</div></template>
+<template>
+	<div>
+		<article>
+			<LazyTemplatesFirstParagraphProjects>
+				<template #logo>
+					<SvgsIconsBildungHerausforderungen
+				/></template>
+				<template #title>Herausforderungen</template>
+				<template #subtitle>Was den Schulbesuch erschwert </template>
+				<template #description
+					>Wir engagieren uns gezielt für einzelne Schulen, indem wir
+					Schulgebäude und -toiletten renovieren und für fehlende
+					Lehrmaterialien Spenden sammeln. Wir vermitteln
+					Partnerschaften zwischen deutschen und kamerunischen
+					Schulen. Und wir unterstützen mittellose Kinder und
+					Jugendliche, indem wir mithilfe von Paten in Deutschland
+					einen kontinuierlichen Schulbesuch ermöglichen.
+				</template>
+				<template #image> </template>
+			</LazyTemplatesFirstParagraphProjects>
+			<div class="px-[1rem] md:px-[3rem]"></div>
+		</article>
+	</div>
+</template>
