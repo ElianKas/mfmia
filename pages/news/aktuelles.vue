c61972ff049d4c90ae047f9a97e4a3d527ffaa7a
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-29 11:12:40 +0100

    news

--- pages/news/aktuelles.vue
+++ pages/news/aktuelles.vue
@@ -28,40 +28,42 @@
 	});
 </script>
 <template>
-	<article class="px-[1rem]">
-		<div
-			class="h-[200px] bg-[#000] w-full rounded-[--border-radius] grid place-items-center my-[1rem]">
-			<h1 class="text-white text-big">Aktuelles</h1>
-		</div>
-		<ul class="my-[5rem] max-w-[900px] m-auto">
-			<ClientOnly>
-				<template #fallback>
-					<div
-						class="skeleton h-[47px] mb-[1rem] w-full"
-						v-for="entry in data.stories.length"></div>
-				</template>
-				<li v-for="article in clientData">
-					<NuxtLink
-						:to="`aktuelles/${article.slug}`"
-						class="p-[1rem] border-b flex justify-between items-center gap-[1rem]">
-						<div>
-							{{
-								new Date(article.content.date).toLocaleDateString(
-									'de-DE',
-									{
-										day: '2-digit',
-										month: '2-digit',
-										year: 'numeric',
-									}
-								)
-							}}
-							<b>{{ article.content.title }}</b>
-						</div>
-						<SvgsNavigationDoubleArrow class="shrink-0"
-					/></NuxtLink>
-				</li>
-			</ClientOnly>
-		</ul>
-	</article>
-	<aside><Newsletter /></aside>
+	<div>
+		<article class="px-[1rem]">
+			<div
+				class="h-[200px] bg-[#000] w-full rounded-[--border-radius] grid place-items-center my-[1rem]">
+				<h1 class="text-white text-big">Aktuelles</h1>
+			</div>
+			<ul class="my-[5rem] max-w-[900px] m-auto">
+				<ClientOnly>
+					<template #fallback>
+						<div
+							class="skeleton h-[47px] mb-[1rem] w-full"
+							v-for="entry in data.stories.length"></div>
+					</template>
+					<li v-for="article in clientData">
+						<NuxtLink
+							:to="`aktuelles/${article.slug}`"
+							class="p-[1rem] border-b flex justify-between items-center gap-[1rem]">
+							<div>
+								{{
+									new Date(article.content.date).toLocaleDateString(
+										'de-DE',
+										{
+											day: '2-digit',
+											month: '2-digit',
+											year: 'numeric',
+										}
+									)
+								}}
+								<b>{{ article.content.title }}</b>
+							</div>
+							<SvgsNavigationDoubleArrow class="shrink-0"
+						/></NuxtLink>
+					</li>
+				</ClientOnly>
+			</ul>
+		</article>
+		<LazyNewsletter />
+	</div>
 </template>
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,11 +1,19 @@
 <template>
-	<article class="px-[1rem]">
-		<div
-			class="h-[200px] bg-[#000] w-full rounded-[--border-radius] grid place-items-center my-[1rem]">
-			<h1 class="text-white text-big">News</h1>
-		</div>
-	</article>
-	<Milestones />
-	<Newsletter />
-	<FewPressreports />
+	<div>
+		<article class="px-[1rem]">
+			<div
+				class="h-[200px] bg-[#000] w-full rounded-[--border-radius] grid place-items-center my-[1rem]">
+				<h1 class="text-white text-big">News</h1>
+			</div>
+			<FewArticles />
+			<div class="text-center pb-[2rem]">
+				<NuxtLink to="/news/aktuelles">
+					<u>Alle Artikel ansehen</u></NuxtLink
+				>
+			</div>
+		</article>
+		<LazyMilestones />
+		<LazyNewsletter />
+		<LazyFewPressreports />
+	</div>
 </template>
