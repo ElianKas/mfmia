17b75acf35fbf69070b973e36fcaacecddd567d0
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-29 20:00:11 +0100

    Update aktiv-werden.vue

--- pages/verein/aktiv-werden.vue
+++ pages/verein/aktiv-werden.vue
@@ -1,1 +1,115 @@
-<template><div>aktiv werden</div></template>
+<template>
+	<div>
+		<article class="px-[1rem] md:px-[3rem] pt-[1rem]">
+			<section>
+				<div
+					class="h-[500px] bg-[#000] grid place-items-center rounded-[--border-radius]">
+					<h1 class="text-white text-big font-bold">Aktiv werden</h1>
+				</div>
+			</section>
+			<TemplatesParagraphsHighlighted>
+				Wir laden Dich herzlich ein, Dich aktiv in unserem Verein zu
+				engagieren und gemeinsam mit uns spannende Projekte
+				voranzutreiben – Deine Ideen und Dein Einsatz machen den
+				Unterschied!
+			</TemplatesParagraphsHighlighted>
+			<section class="py-[2rem] max-w-[800px] m-auto">
+				<h2 class="text-orange border-b border-orange pb-[.5rem]">
+					So kannst du aktiv werden
+				</h2>
+				<!-- item -->
+				<div class="collapse collapse-arrow">
+					<input
+						type="radio"
+						name="my-accordion-2" />
+					<div class="collapse-title text-xl font-medium px-0">
+						<b>Mitglied werden</b>
+					</div>
+					<div class="collapse-content">
+						<p>
+							Du willst aktiv werden?
+							<br />
+							Du möchtest dich engagieren für eine gerechtere Welt?
+							<br />
+							Du interessierst dich für die Verbesserung der
+							Lebensbedingungen in den Dörfern Kameruns?
+							<br /><br />
+							Wir freuen uns, wenn unsere Projekte Anklang bei dir
+							finden. Als Mitglied kannst bei unseren
+							unterschiedlichsten Themen mitmachen, neue Ideen
+							einbringen und den Verein dauerhaft unterstützen.
+							<br /><br />
+							Hier kannst du den Mitgliedsantrag ausfüllen:
+							<br /><br />
+							Der aktuelle Jahresbeitrag einer Mitgliedschaft beträgt
+							60 €.
+						</p>
+					</div>
+				</div>
+				<!-- item -->
+				<div class="collapse collapse-arrow">
+					<input
+						type="radio"
+						name="my-accordion-2" />
+					<div class="collapse-title text-xl font-medium px-0">
+						<b>Pate werden </b>
+					</div>
+					<div class="collapse-content">
+						<p>
+							Du möchtest, dass alle eine Chance auf Bildung haben?
+							<br /><br />
+							Wie wäre es dann mit einer Patenschaft?
+							<br /><br />
+							Wir können nur eine begrenzte Anzahl Patenkinder
+							vermitteln, weil wir persönlich Kontakt halten wollen.
+							Wende dich an uns, wir stellen dir gerne Kinder oder
+							Jugendliche vor, für die wir Paten suchen, damit sie
+							ihre Schul- oder Ausbildung beenden können.
+							<br /><br />
+							Wir kennen jedes Patenkind persönlich und durch einen
+							regelmäßigen Austausch bleibst du als Pate oder Patin
+							immer auf dem Laufenden.
+						</p>
+					</div>
+				</div>
+				<!-- item -->
+				<div class="collapse collapse-arrow">
+					<input
+						type="radio"
+						name="my-accordion-2" />
+					<div class="collapse-title text-xl font-medium px-0">
+						<b>Geld - Spenden helfen! </b>
+					</div>
+					<div class="collapse-content">
+						<p>Inhalt</p>
+					</div>
+				</div>
+				<!-- item -->
+				<div class="collapse collapse-arrow">
+					<input
+						type="radio"
+						name="my-accordion-2" />
+					<div class="collapse-title text-xl font-medium px-0">
+						<b>An einem Projekt mitarbeiten</b>
+					</div>
+					<div class="collapse-content">
+						<p>
+							Du möchtest dich aktiv engagieren?
+							<br /><br />
+							Dann kannst du gerne an den Projekten mitarbeiten und
+							sie voranbringen.
+							<br /><br />
+							Du hast Lust mitzumachen?
+							<br /><br />
+							Nimm dann bitte mit unserem
+							<NuxtLink to="/verein/team"><u>Team</u></NuxtLink>
+							Kontakt auf oder
+							<NuxtLink to="/kontakt"><u>schreib uns</u></NuxtLink
+							>. Wir melden uns bei dir.
+						</p>
+					</div>
+				</div>
+			</section>
+		</article>
+	</div>
+</template>
