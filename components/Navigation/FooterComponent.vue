b1c9fdc7ac74cf433b956a6308a88c65221d1d63
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-29 20:47:36 +0100

    Update FooterComponent.vue

--- components/Navigation/FooterComponent.vue
+++ components/Navigation/FooterComponent.vue
@@ -1,26 +1,59 @@
 <template>
 	<footer>
-		<section class="px-[1rem] py-[3rem] bg-[#000] text-center">
-			<p class="md:max-w-[400px] m-auto text-white">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-				Consequuntur nam voluptates corrupti quam ullam! Quos
-				distinctio cum ipsam dolorum voluptatem at nostrum dolores
-				eligendi est? Quia beatae est impedit vitae?
-			</p>
-			<br />
-			<button class="btn bg-orange text-white border-none">
-				Jetzt spenden
-			</button>
-		</section>
-		<section>
-			<div>
-				<h3 class="text-orange font-bold">Title</h3>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Magni sequi accusantium odio autem. Nisi dignissimos esse
-					est repellat harum perspiciatis in minima odio, aperiam
-					repudiandae ipsam rerum quos impedit tenetur?
+		<section
+			class="px-[1rem] h-[350px] bg-[#000] grid place-items-center text-white px-[1rem] md:px-[3rem]">
+			<div class="text-center">
+				<p class="md:max-w-[400px] m-auto">
+					Helfen Sie mit Ihrer Spende, Projekte zu realisieren, die
+					den Menschen in Kamerun ein unabhängiges und
+					selbstbestimmtes Leben ermöglichen.
 				</p>
+				<br />
+				<button class="btn bg-orange text-white border-none">
+					Jetzt spenden
+				</button>
+				<br />
+				<br />
+				<p>
+					<NuxtLink to="/spenden"><u>Zur Spendenseite</u></NuxtLink>
+				</p>
+			</div>
+		</section>
+		<section
+			class="min-h-[350px] bg-[#2C1200] text-white px-[1rem] md:px-[3rem] py-[2rem]">
+			<div
+				class="max-w-[--max-width] m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem] md:gap-[5rem]">
+				<div class="min-h-[200px]">
+					<h3 class="text-orange font-bold pb[.5rem]">
+						Miteinander - für Menschen in Afrika e.V.
+					</h3>
+					<p>
+						Wir sind ein gemeinnütziger Verein, der sich seit 2013 in
+						Kamerun für Bildung, Gesundheit und Hilfe zur Selbsthilfe
+						einsetzt, um die Lebensbedingungen und Zukunftschancen der
+						lokalen Gemeinschaften nachhaltig zu stärken.
+					</p>
+				</div>
+				<div class="min-h-[200px]">
+					<h3 class="text-orange font-bold pb[.5rem]">
+						Spendenkonto
+					</h3>
+					<p>
+						Miteinander - für Menschen in Afrika e.V. <br />
+						Evangelische Bank <br />
+						BIC: GENODEF1EK1 <br />
+						IBAN: DE02 5206 0410 0000 8044 87 <br />
+					</p>
+				</div>
+				<div class="min-h-[200px]">
+					<h3 class="text-orange font-bold pb[.5rem]">Kontakt</h3>
+					<p>
+						Kölnische Straße 55 <br />
+						D-34117 Kassel <br />
+						Telefon: +49 561-15470 <br />
+						E-Mail: miteinander-fuer-afrika@t-online.de <br />
+					</p>
+				</div>
 			</div>
 		</section>
 	</footer>
