1d333c5b456a59d0953d44582f3e9a77a88fc77b
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-29 20:01:22 +0100

    Update team.vue

--- pages/verein/team.vue
+++ pages/verein/team.vue
@@ -1,1 +1,22 @@
-<template><div>team</div></template>
+<template>
+	<div>
+		<article class="px-[1rem] md:px-[3rem] pt-[1rem]">
+			<section>
+				<div
+					class="h-[500px] bg-[#000] grid place-items-center rounded-[--border-radius]">
+					<h1 class="text-white text-big font-bold">
+						Die Geschichte
+					</h1>
+				</div>
+			</section>
+			<TemplatesParagraphsHighlighted>
+				Unser Team besteht aus vielen Freiwilligen, die ehrenamtlich
+				für den Verein engagieren. Wir kombinieren unsere
+				individuellen Fähigkeiten, um den Menschen in Deutschland ein
+				breites Bild über unsere Arbeit in Kamerun zu vermitteln. Und
+				natürlich, um die Menschen in Kamerun bestmöglich zu
+				unterstützen.
+			</TemplatesParagraphsHighlighted>
+		</article>
+	</div>
+</template>
