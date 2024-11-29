9287581e8119144420aec9ce95d19aa769b34fd3
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-11-29 16:42:10 +0100

    Update FooterComponent.vue

--- components/Navigation/FooterComponent.vue
+++ components/Navigation/FooterComponent.vue
@@ -1,3 +1,27 @@
 <template>
-	<footer class="h-[200px] border"><nav>footer</nav></footer>
+	<footer>
+		<section class="px-[1rem] py-[3rem] bg-[#000] text-center">
+			<p class="md:max-w-[400px] m-auto text-white">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Consequuntur nam voluptates corrupti quam ullam! Quos
+				distinctio cum ipsam dolorum voluptatem at nostrum dolores
+				eligendi est? Quia beatae est impedit vitae?
+			</p>
+			<br />
+			<button class="btn bg-orange text-white border-none">
+				Jetzt spenden
+			</button>
+		</section>
+		<section>
+			<div>
+				<h3 class="text-orange font-bold">Title</h3>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Magni sequi accusantium odio autem. Nisi dignissimos esse
+					est repellat harum perspiciatis in minima odio, aperiam
+					repudiandae ipsam rerum quos impedit tenetur?
+				</p>
+			</div>
+		</section>
+	</footer>
 </template>
