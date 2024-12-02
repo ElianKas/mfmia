5c4b92025979441b97e5b63fac6f2f3b8217791f
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-12-02 12:21:03 +0100

    Update Landing.vue

--- components/Landing.vue
+++ components/Landing.vue
@@ -1,39 +1,15 @@
 <script setup>
 	const gallery = ref(0);
 	let galleryInterval;
-	const img2Src = ref('');
-	const img3Src = ref('');
-	const counter = ref(0);
-
-	function checkState() {
-		if (counter.value === 2) {
-			galleryInterval = setInterval(() => {
-				if (gallery.value < 2) {
-					gallery.value++;
-				} else {
-					gallery.value = 0;
-				}
-			}, 5000);
-		}
-	}
 
 	onMounted(() => {
-		nextTick(() => {
-			const image2 = document.images[1];
-			const image3 = document.images[2];
-			img2Src.value =
-				'https://res.cloudinary.com/dxizsaymj/image/upload/v1731083487/IMG_3529_y4dlkz.jpg';
-			img3Src.value =
-				'https://res.cloudinary.com/dxizsaymj/image/upload/v1731083454/IMG_3611_d9sqye.jpg';
-			image2.onload = () => {
-				counter.value++;
-				checkState();
-			};
-			image3.onload = () => {
-				counter.value++;
-				checkState();
-			};
-		});
+		galleryInterval = setInterval(() => {
+			if (gallery.value < 2) {
+				gallery.value++;
+			} else {
+				gallery.value = 0;
+			}
+		}, 5000);
 	});
 	onBeforeUnmount(() => {
 		clearInterval(galleryInterval);
@@ -43,24 +19,18 @@
 	<section
 		class="p-[1rem] max-w-[500px] min-h-[calc(100vh-80px)] m-auto flex flex-col justify-center">
 		<div class="w-full grow max-h-[500px] relative z-[-99]">
-			<NuxtImg
-				provider="cloudinary"
-				format="avif"
-				src="https://res.cloudinary.com/dxizsaymj/image/upload/v1731083483/IMG_3554_kvdd4f.jpg"
-				class="landing0 absolute inset-0 w-full h-full object-cover rounded-[--border-radius]"
-				:class="{ hidden: gallery !== 0 }" />
-			<NuxtImg
-				provider="cloudinary"
-				format="avif"
-				:src="img2Src"
-				class="landing1 absolute inset-0 w-full h-full object-cover rounded-[--border-radius]"
-				:class="{ hidden: gallery !== 1 }" />
-			<NuxtImg
-				provider="cloudinary"
-				format="avif"
-				:src="img3Src"
-				class="landing2 absolute inset-0 w-full h-full object-cover rounded-[--border-radius]"
-				:class="{ hidden: gallery !== 2 }" />
+			<StoryblokImage
+				:class="{ hidden: gallery !== 0 }"
+				src="https://a.storyblok.com/f/311834/1219x1200/cc4f38bb40/bildung-ubersicht.png"
+				class="landing0 absolute inset-0 w-full h-full object-cover rounded-[--border-radius]" />
+			<StoryblokImage
+				:class="{ hidden: gallery !== 1 }"
+				src="https://a.storyblok.com/f/311834/6000x4000/a72a3107a5/img_3401_wmahxd.jpg"
+				class="landing1 absolute inset-0 w-full h-full object-cover rounded-[--border-radius]" />
+			<StoryblokImage
+				:class="{ hidden: gallery !== 2 }"
+				src="https://a.storyblok.com/f/311834/6000x4000/9da897f2f7/img_3611_1_n9iqtc.jpg"
+				class="landing2 absolute inset-0 w-full h-full object-cover rounded-[--border-radius]" />
 		</div>
 		<br />
 		<SvgsLogoNormal class="w-full h-auto mb-[5rem]" />
