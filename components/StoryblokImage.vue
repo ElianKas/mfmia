6ba81ee0babf78cdfb82265b2316e820faa0d93e
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-12-02 10:37:14 +0100

    Update StoryblokImage.vue

--- components/StoryblokImage.vue
+++ components/StoryblokImage.vue
@@ -16,19 +16,19 @@
 	const baseLink = 'https://a.storyblok.com/f/311834/';
 	const optimizedSrc = ref('');
 
-	function optimize() {
+	function optimize(resolution) {
 		const image = props.src.replace(baseLink, '');
 		const part = image.slice(0, image.indexOf('/'));
 		const [x, y] = part.split('x');
 		if (x > y) {
-			optimizedSrc.value = props.src + '/m/' + '800x0';
+			optimizedSrc.value = props.src + '/m/' + `${resolution}x0`;
 		} else {
-			optimizedSrc.value = props.src + '/m/' + '0x800';
+			optimizedSrc.value = props.src + '/m/' + `0x${resolution}`;
 		}
 	}
 
 	onMounted(() => {
-		optimize();
+		optimize(800);
 	});
 </script>
 <template>
