50d84f5f46d9275ab167c44906f719a07e910c18
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-12-02 19:08:18 +0100

    Update HeroSectionTemplate.vue

--- components/Templates/HeroSectionTemplate.vue
+++ components/Templates/HeroSectionTemplate.vue
@@ -6,10 +6,10 @@
 				<slot name="image"></slot>
 			</div>
 			<h1
-				class="text-big text-white font-bold md:absolute md:left-[50%] md:top-[50%] md:translate-x-[-50%] md:translate-y-[-50%]">
+				class="text-big text-white font-bold md:absolute md:left-[50%] md:top-[50%] md:translate-x-[-50%] md:translate-y-[-50%] z-[9]">
 				<slot name="title"></slot>
 			</h1>
-			<div class="flex flex-col md:flex-row gap-[2rem] z-[1]">
+			<div class="flex flex-col md:flex-row gap-[2rem] z-[9]">
 				<p class="font-bold text-white md:text-right max-w-[300px]">
 					<slot name="description"></slot>
 				</p>
