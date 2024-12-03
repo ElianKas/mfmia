5a799298df83a3a95b9fbdad948c3c569c55405a
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-12-03 10:38:33 +0100

    Create Hygiene.vue

---
+++ components/Svgs/Icons/Hygiene/Hygiene.vue
@@ -0,0 +1,63 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		width="120"
+		height="120"
+		viewBox="0 0 150 150">
+		<defs>
+			<linearGradient
+				id="a"
+				x1="1"
+				x2="0"
+				y2="1"
+				gradientUnits="objectBoundingBox">
+				<stop
+					offset="0"
+					stop-color="#d9345a" />
+				<stop
+					offset="1"
+					stop-color="#d63ea7" />
+			</linearGradient>
+		</defs>
+		<g transform="translate(-59 -836)">
+			<rect
+				width="150"
+				height="150"
+				rx="10"
+				transform="translate(59 836)"
+				fill="url(#a)" />
+			<g transform="translate(-0.191 0.257)">
+				<path
+					d="M11714,3161.653s29.781-22.339,34.2-37.764-8.312-24.107-16.54-23.936-17.655,12.155-17.655,12.155-10.111-13.7-18.837-12.155-19.44,9.5-14.731,24.923S11714,3161.653,11714,3161.653Z"
+					transform="translate(-11580 -2220)"
+					fill="none"
+					stroke="#fff"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2" />
+				<path
+					d="M11709.424,3127.882l.532-9.836h8.535v9.836h9.6v8.889h-9.6v9.1h-9.067v-9.772h-9.072v-8.213Z"
+					transform="translate(-11580 -2220)"
+					fill="none"
+					stroke="#fff"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2" />
+				<circle
+					cx="1.5"
+					cy="1.5"
+					r="1.5"
+					transform="translate(157 887)"
+					fill="#fff" />
+				<path
+					d="M11741.207,3111.783a14.594,14.594,0,0,1,1.843,9.942c-1.113,5.383-6.3,11.589-6.3,11.589"
+					transform="translate(-11580 -2220)"
+					fill="none"
+					stroke="#fff"
+					stroke-linecap="round"
+					stroke-width="2" />
+			</g>
+		</g>
+	</svg>
+</template>
