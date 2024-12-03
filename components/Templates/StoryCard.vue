6c4c7d9fb1f4ccc0f34b57ace7a503eda412e25a
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-12-03 21:08:38 +0100

    Update StoryCard.vue

--- components/Templates/StoryCard.vue
+++ components/Templates/StoryCard.vue
@@ -7,17 +7,19 @@
 	<NuxtLink
 		v-if="story"
 		:to="story.full_slug"
-		class="h-[500px] w-full rounded-[--border-radius] relative">
+		class="h-[500px] w-full rounded-[--border-radius] relative flex items-end">
 		<StoryblokImage
 			class="rounded-[--border-radius] absolute inset-0 w-full h-full object-cover"
 			:src="story.content.cover.filename"
 			:alt="story.content.title" />
 		<div
-			class="backdrop-blur h-[150px] p-[1rem] absolute text-white bottom-0 w-full rounded-b-[--border-radius] flex flex-col justify-between">
+			class="backdrop-blur h-[180px] p-[1rem] relative text-white w-full rounded-b-[--border-radius] flex flex-col justify-between">
 			<p class="font-bold">
 				{{ story.content.title }}
 			</p>
-			<div class="self-end"><SvgsNavigationShortArrow /></div>
+			<div class="absolute bottom-[1rem] right-[1rem]">
+				<SvgsNavigationShortArrow />
+			</div>
 		</div>
 	</NuxtLink>
 </template>
