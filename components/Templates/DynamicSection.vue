455d0f2e01fbd066ba15e3c9a3534da7732f1bc2
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-12-05 11:42:33 +0100

    Update DynamicSection.vue

--- components/Templates/DynamicSection.vue
+++ components/Templates/DynamicSection.vue
@@ -1,6 +1,5 @@
 <script setup>
 	const props = defineProps({ section: Object });
-	const currentSlide = ref(0);
 	const descriptionContent = computed(() => {
 		if (props.section.description.type === 'doc') {
 			return renderRichText(props.section.description);
@@ -8,6 +7,7 @@
 			return props.section.description;
 		}
 	});
+	const currentIndex = ref(0);
 </script>
 <template>
 	<section class="py-[1.5rem]">
@@ -17,6 +17,29 @@
 			:src="section.gallery[0].image"
 			:alt="section.gallery[0].alt"
 			class="w-full mb-[1rem] max-h-[500px] object-cover" />
+		<div
+			v-if="section.gallery.length > 1"
+			class="mb-[1rem]">
+			<StoryblokImage
+				v-for="(image, index) in section.gallery"
+				:src="image.image"
+				:alt="image.alt"
+				:class="{ hidden: index !== currentIndex }"
+				class="w-full mb-[1rem] max-h-[500px] object-cover" />
+			<div class="w-full flex flex-wrap gap-[.5rem]">
+				<StoryblokImage
+					v-for="(image, index) in section.gallery"
+					:key="image"
+					:src="image.image"
+					:alt="image.alt"
+					@click="
+						() => {
+							currentIndex = index;
+						}
+					"
+					class="w-[100px] aspect-[3/2] object-cover cursor-pointer" />
+			</div>
+		</div>
 		<div class="max-w-[800px] m-auto">
 			<h2
 				class="font-bold text-big text-green"
