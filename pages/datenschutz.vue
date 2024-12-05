833460e7a0fe78e68d1d2c865b8ffab9c33d1751
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-12-05 18:31:07 +0100

    Update datenschutz.vue

--- pages/datenschutz.vue
+++ pages/datenschutz.vue
@@ -1,1 +1,338 @@
-<template><div>datenschutz</div></template>
+<template>
+	<div>
+		<article
+			class="min-h-[100vh] px-[1rem] md:px-[3rem] max-w-[--max-width] m-auto py-[5rem]">
+			<h1 class="text-big font-bold text-green pb-[2rem]">
+				Impressum
+			</h1>
+			<div class="flex flex-col gap-[1rem]">
+				<h2>
+					<b
+						>1. Name und Kontaktdaten des für die Verarbeitung
+						Verantwortlichen</b
+					>
+				</h2>
+				<p>
+					Diese Datenschutz-Information gilt für die Datenverarbeitung
+					durch:
+				</p>
+				<p>
+					Miteinander - für Menschen in Afrika e.V., Kölnische Straße
+					55, D-34117 Kassel, Telefon: +49 561-15470, Telefax: +49
+					561-779477, E-Mail: pfarrei@st-familia-kassel.de
+				</p>
+
+				<h2>
+					<b
+						>2. Erhebung und Speicherung personenbezogener Daten sowie
+						Art und Zweck von deren Verwendung</b
+					>
+				</h2>
+				<h3><b>a) Beim Besuch der Website</b></h3>
+				<p>
+					Beim Aufrufen unserer Website www.miteinander-fuer-afrika.de
+					werden durch den auf Ihrem Endgerät zum Einsatz kommenden
+					Browser automatisch Informationen an den Server unserer
+					Website gesendet. Diese Informationen werden temporär in
+					einem sog. Logfile gespeichert. Folgende Informationen
+					werden dabei ohne Ihr Zutun erfasst und bis zur
+					automatisierten Löschung gespeichert:
+				</p>
+				<ul class="list-disc pl-[1rem]">
+					<li>IP-Adresse des anfragenden Rechners,</li>
+					<li>Datum und Uhrzeit des Zugriffs,</li>
+					<li>Name und URL der abgerufenen Datei,</li>
+					<li>
+						Website, von der aus der Zugriff erfolgt (Referrer-URL),
+					</li>
+					<li>
+						verwendeter Browser und ggf. das Betriebssystem Ihres
+						Rechners sowie der Name Ihres Access-Providers.
+					</li>
+				</ul>
+				<p>
+					Die genannten Daten werden durch uns zu folgenden Zwecken
+					verarbeitet:
+				</p>
+				<ul class="list-disc pl-[1rem]">
+					<li>
+						Gewährleistung eines reibungslosen Verbindungsaufbaus der
+						Website,
+					</li>
+					<li>
+						Gewährleistung einer komfortablen Nutzung unserer Website,
+					</li>
+					<li>
+						Auswertung der Systemsicherheit und -stabilität sowie
+					</li>
+					<li>zu weiteren administrativen Zwecken.</li>
+				</ul>
+				<p>
+					Die Rechtsgrundlage für die Datenverarbeitung ist Art. 6
+					Abs. 1 S. 1 lit. f DSGVO. Unser berechtigtes Interesse folgt
+					aus oben aufgelisteten Zwecken zur Datenerhebung. In keinem
+					Fall verwenden wir die erhobenen Daten zu dem Zweck,
+					Rückschlüsse auf Ihre Person zu ziehen.
+				</p>
+
+				<h3><b>b) Bei Nutzung unseres Kontaktformulars</b></h3>
+				<p>
+					Bei Fragen jeglicher Art bieten wir Ihnen die Möglichkeit,
+					mit uns über ein auf der Website bereitgestelltes Formular
+					Kontakt aufzunehmen. Dabei ist die Angabe einer gültigen
+					E-Mail-Adresse erforderlich, damit wir wissen, von wem die
+					Anfrage stammt und um diese beantworten zu können. Weitere
+					Angaben können freiwillig getätigt werden.
+				</p>
+				<p>
+					Die Datenverarbeitung zum Zwecke der Kontaktaufnahme mit uns
+					erfolgt nach Art. 6 Abs. 1 S. 1 lit. a DSGVO auf Grundlage
+					Ihrer freiwillig erteilten Einwilligung.
+				</p>
+				<p>
+					Die für die Benutzung des Kontaktformulars von uns erhobenen
+					personenbezogenen Daten werden nach Erledigung der von Ihnen
+					gestellten Anfrage automatisch gelöscht.
+				</p>
+
+				<h2><b>3. Weitergabe von Daten</b></h2>
+				<p>
+					Eine Übermittlung Ihrer persönlichen Daten an Dritte zu
+					anderen als den im Folgenden aufgeführten Zwecken findet
+					nicht statt.
+				</p>
+				<p>
+					Wir geben Ihre persönlichen Daten nur an Dritte weiter,
+					wenn:
+				</p>
+				<ul class="list-disc pl-[1rem]">
+					<li>
+						Sie Ihre nach Art. 6 Abs. 1 S. 1 lit. a DSGVO
+						ausdrückliche Einwilligung dazu erteilt haben,
+					</li>
+					<li>
+						die Weitergabe nach Art. 6 Abs. 1 S. 1 lit. f DSGVO zur
+						Geltendmachung, Ausübung oder Verteidigung von
+						Rechtsansprüchen erforderlich ist und kein Grund zur
+						Annahme besteht, dass Sie ein überwiegendes schutzwürdiges
+						Interesse an der Nichtweitergabe Ihrer Daten haben,
+					</li>
+					<li>
+						für den Fall, dass für die Weitergabe nach Art. 6 Abs. 1
+						S. 1 lit. c DSGVO eine gesetzliche Verpflichtung besteht,
+						sowie
+					</li>
+					<li>
+						dies gesetzlich zulässig und nach Art. 6 Abs. 1 S. 1 lit.
+						b DSGVO für die Abwicklung von Vertragsverhältnissen mit
+						Ihnen erforderlich ist.
+					</li>
+				</ul>
+
+				<h2><b>4. Cookies</b></h2>
+				<p>
+					Wir setzen auf unserer Website ausschließlich technisch
+					notwendige, sogenannte Session-Cookies ein, die nur für die
+					aktuelle Browsersitzung bestehen bleiben. Hierbei handelt es
+					sich um kleine Dateien, die Ihr Browser automatisch erstellt
+					und die auf Ihrem Endgerät (Laptop, Tablet, Smartphone o.ä.)
+					gespeichert werden, wenn Sie unsere Internetseiten besuchen.
+					Generell richten Cookies auf Ihrem Endgerät keinen Schaden
+					an, enthalten keine Viren, Trojaner oder sonstige
+					Schadsoftware. Beim Verlassen unserer Website werden diese
+					Session-Cookies automatisch wieder gelöscht.
+				</p>
+				<p>
+					Der Einsatz von Cookies dient einerseits dazu, die Nutzung
+					unseres Angebots für Sie angenehmer zu gestalten bzw. die
+					einwandfreie Funktionalität zu gewährleisten.
+				</p>
+				<p>
+					Die meisten Browser akzeptieren Cookies automatisch. Sie
+					können Ihren Browser jedoch so konfigurieren, dass keine
+					Cookies auf Ihrem Computer gespeichert werden oder stets ein
+					Hinweis erscheint, bevor ein neuer Cookie angelegt wird. Die
+					vollständige Deaktivierung von Cookies kann jedoch dazu
+					führen, dass Sie nicht alle Funktionen unserer Website
+					nutzen können.
+				</p>
+
+				<h2><b>5. YouTube</b></h2>
+				<p>
+					Wir setzen auf unseren Internetseiten Videos des
+					Videoportals „YouTube“ des Unternehmens Google Ireland
+					Limited, Gordon House, Barrow Street, Dublin 4, Irland (Im
+					Folgenden: „Google“) ein. Die Implementierung erfolgt
+					aufgrund von Art. 6 Abs. 1 S. 1 lit. f DSGVO, wobei unser
+					Interesse in der reibungslosen Integration der Videos und
+					der damit ansprechenden Gestaltung unserer Webseite liegt.
+				</p>
+				<p>
+					Hierbei nutzen wir die von Google zur Verfügung gestellte
+					Option des erweiterten Datenschutzmodus.
+				</p>
+				<p>
+					Wenn Sie eine Seite aufrufen, die über ein eingebettetes
+					Video verfügt, wird eine Verbindung zu den Google-Servern
+					hergestellt und dabei der Inhalt durch Mitteilung an Ihren
+					Browser auf der Internetseite dargestellt.
+				</p>
+				<p>
+					Laut den Angaben von Google werden im erweiterten
+					Datenschutzmodus nur dann Ihre Daten – insbesondere welche
+					unserer Internetseiten Sie besucht haben sowie
+					gerätespezifische Informationen inklusive der IP-Adresse –
+					an den YouTube-Server in den USA übermittelt, wenn Sie das
+					Video anschauen. Durch den Klick auf das Video willigen Sie
+					in diese Übermittlung ein.
+				</p>
+				<p>
+					Sind Sie gleichzeitig bei Google eingeloggt, werden diese
+					Informationen Ihrem Mitgliedskonto bei YouTube zugeordnet.
+					Dies können Sie verhindern, indem Sie sich vor dem Besuch
+					unserer Website von Ihrem Mitgliedskonto abmelden.
+				</p>
+				<p>
+					Teilweise werden Informationen an die Muttergesellschaft
+					Google Inc. mit Sitz in den USA, an andere
+					Google-Unternehmen und an externe Partner von Google
+					übermittelt, die sich jeweils außerhalb der Europäischen
+					Union befinden können. Google verwendet dafür von der
+					Europäischen Kommission genehmigte Standardvertragsklauseln
+					und verlässt sich auf die von der Europäischen Kommission
+					erlassenen Angemessenheitsbeschlüsse bezüglich bestimmter
+					Länder.
+				</p>
+				<p>
+					Weitere Informationen zum Datenschutz im Zusammenhang mit
+					YouTube finden Sie in den Datenschutzbestimmungen von
+					Google.
+				</p>
+
+				<h2><b>6. Betroffenenrechte</b></h2>
+				<p>Sie haben das Recht:</p>
+				<ul class="list-disc pl-[1rem]">
+					<li>
+						gemäß Art. 15 DSGVO Auskunft über Ihre von uns
+						verarbeiteten personenbezogenen Daten zu verlangen.
+						Insbesondere können Sie Auskunft über die
+						Verarbeitungszwecke, die Kategorie der personenbezogenen
+						Daten, die Kategorien von Empfängern, gegenüber denen Ihre
+						Daten offengelegt wurden oder werden, die geplante
+						Speicherdauer, das Bestehen eines Rechts auf Berichtigung,
+						Löschung, Einschränkung der Verarbeitung oder Widerspruch,
+						das Bestehen eines Beschwerderechts, die Herkunft ihrer
+						Daten, sofern diese nicht bei uns erhoben wurden, sowie
+						über das Bestehen einer automatisierten
+						Entscheidungsfindung einschließlich Profiling und ggf.
+						aussagekräftigen Informationen zu deren Einzelheiten
+						verlangen;
+					</li>
+					<li>
+						gemäß Art. 16 DSGVO unverzüglich die Berichtigung
+						unrichtiger oder Vervollständigung Ihrer bei uns
+						gespeicherten personenbezogenen Daten zu verlangen;
+					</li>
+					<li>
+						gemäß Art. 17 DSGVO die Löschung Ihrer bei uns
+						gespeicherten personenbezogenen Daten zu verlangen, soweit
+						nicht die Verarbeitung zur Ausübung des Rechts auf freie
+						Meinungsäußerung und Information, zur Erfüllung einer
+						rechtlichen Verpflichtung, aus Gründen des öffentlichen
+						Interesses oder zur Geltendmachung, Ausübung oder
+						Verteidigung von Rechtsansprüchen erforderlich ist;
+					</li>
+					<li>
+						gemäß Art. 18 DSGVO die Einschränkung der Verarbeitung
+						Ihrer personenbezogenen Daten zu verlangen, soweit die
+						Richtigkeit der Daten von Ihnen bestritten wird, die
+						Verarbeitung unrechtmäßig ist, Sie aber deren Löschung
+						ablehnen und wir die Daten nicht mehr benötigen, Sie
+						jedoch diese zur Geltendmachung, Ausübung oder
+						Verteidigung von Rechtsansprüchen benötigen oder Sie gemäß
+						Art. 21 DSGVO Widerspruch gegen die Verarbeitung eingelegt
+						haben;
+					</li>
+					<li>
+						gemäß Art. 20 DSGVO Ihre personenbezogenen Daten, die Sie
+						uns bereitgestellt haben, in einem strukturierten,
+						gängigen und maschinenlesebaren Format zu erhalten oder
+						die Übermittlung an einen anderen Verantwortlichen zu
+						verlangen;
+					</li>
+					<li>
+						gemäß Art. 7 Abs. 3 DSGVO Ihre einmal erteilte
+						Einwilligung jederzeit gegenüber uns zu widerrufen. Dies
+						hat zur Folge, dass wir die Datenverarbeitung, die auf
+						dieser Einwilligung beruhte, für die Zukunft nicht mehr
+						fortführen dürfen und
+					</li>
+					<li>
+						gemäß Art. 77 DSGVO sich bei einer Aufsichtsbehörde zu
+						beschweren. In der Regel können Sie sich hierfür an die
+						Aufsichtsbehörde Ihres üblichen Aufenthaltsortes oder
+						Arbeitsplatzes oder unseres Firmensitzes wenden.
+					</li>
+				</ul>
+				<h2><b>7. Widerspruchsrecht</b></h2>
+				<p>
+					Sofern Ihre personenbezogenen Daten auf Grundlage von
+					berechtigten Interessen gemäß Art. 6 Abs. 1 S. 1 lit. f
+					DSGVO verarbeitet werden, haben Sie das Recht, gemäß Art. 21
+					DSGVO Widerspruch gegen die Verarbeitung Ihrer
+					personenbezogenen Daten einzulegen, soweit dafür Gründe
+					vorliegen, die sich aus Ihrer besonderen Situation ergeben
+					oder sich der Widerspruch gegen Direktwerbung richtet. Im
+					letzteren Fall haben Sie ein generelles Widerspruchsrecht,
+					das ohne Angabe einer besonderen Situation von uns umgesetzt
+					wird. Möchten Sie von Ihrem Widerrufs- oder
+					Widerspruchsrecht Gebrauch machen, genügt eine E-Mail an
+					<span class="text-orange"
+						>info[at]miteinander-fuer-afrika.de</span
+					>
+				</p>
+
+				<h2><b>8. Datensicherheit</b></h2>
+				<p>
+					Wir verwenden innerhalb des Website-Besuchs das verbreitete
+					SSL-Verfahren (Secure Socket Layer) in Verbindung mit der
+					jeweils höchsten Verschlüsselungsstufe, die von Ihrem
+					Browser unterstützt wird. In der Regel handelt es sich dabei
+					um eine 256 Bit Verschlüsselung. Falls Ihr Browser keine
+					256-Bit Verschlüsselung unterstützt, greifen wir stattdessen
+					auf 128-Bit v3 Technologie zurück. Ob eine einzelne Seite
+					unseres Internetauftrittes verschlüsselt übertragen wird,
+					erkennen Sie an der geschlossenen Darstellung des Schüssel-
+					beziehungsweise Schloss-Symbols in der unteren Statusleiste
+					Ihres Browsers.
+					<br /><br />
+					Wir bedienen uns im Übrigen geeigneter technischer und
+					organisatorischer Sicherheitsmaßnahmen, um Ihre Daten gegen
+					zufällige oder vorsätzliche Manipulationen, teilweisen oder
+					vollständigen Verlust, Zerstörung oder gegen den unbefugten
+					Zugriff Dritter zu schützen. Unsere Sicherheitsmaßnahmen
+					werden entsprechend der technologischen Entwicklung
+					fortlaufend verbessert.
+				</p>
+
+				<h2>
+					<b
+						>9. Aktualität und Änderung dieser Datenschutzerklärung</b
+					>
+				</h2>
+				<p>
+					Diese Datenschutzerklärung ist aktuell gültig und hat den
+					Stand Mai 2024.
+					<br /><br />
+					Durch die Weiterentwicklung unserer Website und Angebote
+					darüber oder aufgrund geänderter gesetzlicher
+					beziehungsweise behördlicher Vorgaben kann es notwendig
+					werden, diese Datenschutzerklärung zu ändern. Die jeweils
+					aktuelle Datenschutzerklärung kann jederzeit auf der Website
+					unter https://miteinander-fuer-afrika.de/datenschutz.html
+					von Ihnen abgerufen und ausgedruckt werden.
+				</p>
+			</div>
+		</article>
+	</div>
+</template>
