027d5c1438303befcf8108742cd4c461e9725d6c
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-12-05 17:57:36 +0100

    Update impressum.vue

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,1 +1,96 @@
-<template><div>impressum</div></template>
+<template>
+	<div>
+		<article
+			class="min-h-[100vh] px-[1rem] md:px-[3rem] max-w-[--max-width] m-auto py-[5rem]">
+			<h1 class="text-big font-bold text-green pb-[2rem]">
+				Impressum
+			</h1>
+			<div class="grid grid-cols-1 md:grid-cols-2 gap-[2rem]">
+				<div class="flex flex-col gap-[2rem]">
+					<div>
+						<h3 class="font-bold pb-[.5rem]">Angaben nach § 5 TMG</h3>
+						<p>
+							Miteinander - für Menschen in Afrika e.V. <br />
+							Kölnische Straße 55 <br />
+							D-34117 Kassel
+						</p>
+					</div>
+					<div>
+						<h3 class="font-bold pb-[.5rem]">Kontakt</h3>
+						<p>
+							Telefon: +49 561-15470 <br />
+							Telefax: +49 561-779477 <br />
+							E-Mail: info@miteinander-fuer-afrika.de
+						</p>
+					</div>
+					<div>
+						<h3 class="font-bold pb-[.5rem]">Vereinsregister</h3>
+						<p>Amsgericht Kassel - Registergerichtseintrag VR 5044</p>
+					</div>
+					<div>
+						<h3 class="font-bold pb-[.5rem]">
+							Vertretungsberechtigter Vorstand
+						</h3>
+						<p>Andreas Wirtz, Harald Fischer</p>
+					</div>
+					<div>
+						<h3 class="font-bold pb-[.5rem]">
+							Verantwortlich für den Inhalt
+						</h3>
+						<p>Andreas Wirtz, Harald Fischer</p>
+					</div>
+					<div>
+						<h3 class="font-bold pb-[.5rem]">
+							Vereins- und Spendenkonto
+						</h3>
+						<p>
+							Evangelische Bank <br />
+							BIC: GENODEF1EK1 <br />
+							IBAN: DE02 5206 0410 0000 8044 87
+						</p>
+					</div>
+				</div>
+				<div class="flex flex-col gap-[2rem]">
+					<div>
+						<h3 class="font-bold pb-[.5rem]">
+							Konzeption & Gestaltung & Programmierung
+						</h3>
+						<p>
+							apesolute | Digital DNA <br />
+							<NuxtLink
+								to="https://apesolute.com/"
+								target="_blank"
+								><u>www.apesolute.com</u></NuxtLink
+							>
+							<br />
+							langhein[at]apesolute.com
+						</p>
+					</div>
+					<div>
+						<h3 class="font-bold pb-[.5rem]">Redaktionsteam</h3>
+						<p>
+							Sibylle Lachmitz <br />
+							Andreas Wirtz <br />
+							Elian Kasteleiner <br />
+							Thomas Langhein <br />
+							Lukas Langhein
+						</p>
+					</div>
+					<div>
+						<h3 class="font-bold pb-[.5rem]">Fotografien</h3>
+						<p>
+							Dina Falten <br />
+							Martin Schwegmann <br />
+							Andreas Wirtz <br />
+							Verein “Miteinander - für Menschen in Afrika e.V.”
+						</p>
+					</div>
+					<div>
+						<h3 class="font-bold pb-[.5rem]">Texte</h3>
+						<p>Dina Falten, Sibylle Lachmitz, Andreas Wirtz</p>
+					</div>
+				</div>
+			</div>
+		</article>
+	</div>
+</template>
