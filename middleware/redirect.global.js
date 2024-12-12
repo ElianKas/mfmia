de3c57ed9868fb9f12994deffc36708b6256fad5
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-12-12 16:19:29 +0100

    Update redirect.global.js

--- middleware/redirect.global.js
+++ middleware/redirect.global.js
@@ -1,31 +1,77 @@
 // middleware/redirect.js
 export default defineNuxtRouteMiddleware((to, from) => {
-	if (to.path === '/aktuelles') {
+	if (to.path === '/aktuelles.html') {
 		return navigateTo('/news/aktuelles');
 	}
-	if (to.path === '/kamerun') {
+	if (to.path === '/kamerun.html') {
 		return navigateTo('/projekte/kamerun');
 	}
-	if (to.path === '/toilettenhaeuser') {
+	if (to.path === '/toilettenhaeuser.html') {
 		return navigateTo(
 			'/projekte/hygiene-gesundheit/toilettenhaeuser'
 		);
 	}
-	if (to.path === '/toilettenhaeuser') {
-		return navigateTo(
-			'/projekte/hygiene-gesundheit/toilettenhaeuser'
-		);
-	}
-	if (to.path === '/brunnenbau') {
+	if (to.path === '/brunnenbau.html') {
 		return navigateTo('/projekte/brunnenbau');
 	}
-	if (to.path === '/patenschaften') {
+	if (to.path === '/patenschaften.html') {
 		return navigateTo('/projekte/bildung/patenschaften');
 	}
-	if (to.path === '/bildungssystem') {
+	if (to.path === '/bildungssystem.html') {
 		return navigateTo('/projekte/bildung/schulsystem');
 	}
-	if (to.path === '/schulen') {
+	if (to.path === '/schulen.html') {
 		return navigateTo('/projekte/bildung/schulenbau');
+	}
+	if (to.path === '/solarlight.html') {
+		return navigateTo('/projekte/hilfe-zur-selbsthilfe/solar-light');
+	}
+	if (to.path === '/plantage.html') {
+		return navigateTo('/projekte/hilfe-zur-selbsthilfe/plantagen');
+	}
+	if (to.path === '/aktionen.html') {
+		return navigateTo('/news');
+	}
+	if (to.path === '/spenden.html') {
+		return navigateTo('/spenden');
+	}
+	if (to.path === '/mitglied-werden.html') {
+		return navigateTo('/verein/aktiv-werden');
+	}
+	if (to.path === '/pate-werden.html') {
+		return navigateTo('/verein/aktiv-werden');
+	}
+	if (to.path === '/unser-verein.html') {
+		return navigateTo('/verein');
+	}
+	if (to.path === '/die-idee.html') {
+		return navigateTo('/verein/geschichte');
+	}
+	if (to.path === '/das-team.html') {
+		return navigateTo('/verein/team');
+	}
+	if (to.path === '/reiseberichte-38.html') {
+		return navigateTo('/storys');
+	}
+	if (to.path === '/presseberichte.html') {
+		return navigateTo('/news/presse');
+	}
+	if (to.path === '/kontakt.html') {
+		return navigateTo('/kontakt');
+	}
+	/* if (to.path === '/vereinschronik.html') {
+		return navigateTo('/verein');
+		} */
+	/* if (to.path === '/kinder-und-frauenzentrum.html') {
+		return navigateTo('/projekte/hilfe-zur-selbsthilfe/plantagen');
+		} */
+	if (to.path === '/impressum.html') {
+		return navigateTo('/impressum');
+	}
+	/* if (to.path === '/disclaimer.html') {
+		return navigateTo('/impressum');
+		} */
+	if (to.path === '/datenschutz.html') {
+		return navigateTo('/datenschutz');
 	}
 });
