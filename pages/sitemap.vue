06cde21a5cba317c2e36a8a0046cc18e98c2b973
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-12-13 11:45:15 +0100

    sitemap

--- pages/sitemap.vue
+++ pages/sitemap.vue
@@ -3,4 +3,12 @@
 		title: 'Sitemap',
 	});
 </script>
-<template></template>
+<template>
+	<div>
+		<article
+			class="min-h-[100vh] px-[1rem] md:px-[3rem] max-w-[--max-width] m-auto py-[5rem]">
+			<h1 class="text-big font-bold text-green pb-[2rem]">Sitemap</h1>
+			<div class="flex flex-col gap-[1rem]"></div>
+		</article>
+	</div>
+</template>
