67b14b46ed428769d85b1092c4ce4154783d7692
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-12-13 12:26:50 +0100

    favicon

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 	import { Analytics } from '@vercel/analytics/nuxt';
+	useSeoMeta({
+		title: 'Miteinander - für Menschen in Afrika e.V.',
+		ogTitle: 'Miteinander - für Menschen in Afrika e.V.',
+		description:
+			'Gemeinsam für Bildung, Gesundheit und Selbsthilfe in Kamerun. Erfahren Sie mehr über unsere aktuellen Projekte und wie Sie helfen können.',
+		ogDescription:
+			'Gemeinsam für Bildung, Gesundheit und Selbsthilfe in Kamerun. Erfahren Sie mehr über unsere aktuellen Projekte und wie Sie helfen können.',
+		ogImage: 'https://example.com/image.png',
+		twitterCard: 'summary_large_image',
+	});
 </script>
 <template>
 	<Analytics />
