0bcc312e20ccc32f6a84f9ab4a97a2666c45e7da
ElianKas <135838274+ElianKas@users.noreply.github.com>
2024-12-13 12:12:59 +0100

    analytics

--- app.vue
+++ app.vue
@@ -1,4 +1,8 @@
+<script setup lang="ts">
+	import { Analytics } from '@vercel/analytics/nuxt';
+</script>
 <template>
+	<Analytics />
 	<div class="relative">
 		<NavigationHeaderComponent />
 		<main><NuxtPage /></main>
